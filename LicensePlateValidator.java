f8032cc201dc99a875e4280caad1d7c6338c0a54
Ambuj Singh <joy212ambuj@gmail.com>
2025-02-25 22:24:11 +0530

    22-feb-2025-a

--- LicensePlateValidator.java
+++
@@ -1,8 +0,0 @@
-class LicensePlateValidator {
-    public static boolean isValid(String plate) {
-        return plate.matches("^[A-Z]{2}\\d{4}$");
-    }
-    public static void main(String[] args) {
-        System.out.println(isValid("AB1234"));
-    }
-}
